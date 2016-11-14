2ed9e6d88fd18d2ff64d78e81b31f5374ccae411
luzhenghao <luzhenghao_ios@etudetime.com>
2016-11-14 22:34:38 +0800

    update framwork

--- LuBottomAlertView/LuBottomAlertView/ViewController.swift
+++ LuBottomAlertView/LuBottomAlertView/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    lazy var testTableView : UITableView = {
+        var testTableView = UITableView()
+        testTableView.backgroundColor = UIColor.lightGray
+        testTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        testTableView.delegate   = self
+        testTableView.dataSource = self
+        return testTableView
+    }()
+    
+    let textArr = ["弹出相同样式列表",
+                   "弹出上灰下红样式列表",
+                   "弹出时间轴"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        view.addSubview(testTableView)
+        testTableView.snp.makeConstraints { (make) in
+            make.left.right.top.bottom.equalToSuperview()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+extension ViewController{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
 
-
+        cell.textLabel?.text = textArr[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        debugPrint("\(indexPath)")
+        switch indexPath.row {
+        case 0:
+            let str = ["0","测试测试测试","测试测试测试"]
+            LuBottomAlertView().showBottomTableView(indexTextArr: str, bottomAction: { (num) in
+                debugPrint("所选角标:\(num!)")
+            })
+            break
+        case 1:
+            let str = ["1","测试测试测试","测试测试测试"]
+            LuBottomAlertView().showBottomTableView(indexTextArr: str, bottomAction: { (num) in
+                debugPrint("所选角标:\(num!)")
+            })
+            break
+        case 2:
+            
+            break
+        default:
+            break
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return (UIScreen.main.bounds.size.height-64)/5
+    }
 }
 
---
+++ LuBottomAlertView/Pods/Target Support Files/Pods-LuBottomAlertView/Pods-LuBottomAlertView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LuBottomAlertView : NSObject
+@end
+@implementation PodsDummy_Pods_LuBottomAlertView
+@end
